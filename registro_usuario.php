cd3d65b7dce3e698f6bbfdf1e07ecce9238b600c
EcoGarcia101 <611910454@utsalamanca.edu.mx>
2024-01-16 09:17:55 -0600

    Se agrego que se registre un admin

--- registro_usuario.php
+++ registro_usuario.php
@@ -11,16 +11,17 @@ include('includes/conexion.php');
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $nombre = $_POST['nombre'];
     $password = $_POST['password'];
+    $email = $_POST['email'];
     $tipo_usuario = $_POST['tipo_usuario'];
 
     // Hash de la contraseña (puedes utilizar algoritmos más seguros)
     $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
     // Consulta SQL para insertar un nuevo usuario
-    $consulta = "INSERT INTO usuarios (FullName, Passwo, Puesto) VALUES (?, ?, ?)";
+    $consulta = "INSERT INTO usuarios (FullName, Passwo, Puesto, EmailId) VALUES (?, ?, ?, ?)";
     $stmt = $conexion->prepare($consulta);
-    $stmt->bind_param("ssi", $nombre, $hashed_password, $tipo_usuario);
-
+    $stmt->bind_param("ssis", $nombre, $hashed_password, $tipo_usuario, $email);
+    
     if ($stmt->execute()) {
         echo "Usuario registrado correctamente.";
         // Puedes redirigir a otra página si lo deseas
@@ -49,7 +50,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <form method="post" action="">
         <label for="nombre">Nombre:</label>
         <input type="text" name="nombre" required>
-        <br>
+
+        <label for="nombre">Correo electronico:</label>
+        <input type="text" name="email" required>
+
+
+        <br> 
         <label for="password">Contraseña:</label>
         <input type="password" name="password" required>
         <br>
