7814fdf25ac50021b8068a85689feccbd031f5d5
EcoGarcia101 <611910454@utsalamanca.edu.mx>
2024-01-16 09:40:06 -0600

    Modificació de lista para que se vea el header en ese archivo

--- lista.php
+++ lista.php
@@ -1,6 +1,12 @@
 <?php
 session_start();
-error_reporting(0);
+
+if (!isset($_SESSION['tipo_usuario'])) {
+    header('Location: index.php');
+    exit();
+}
+
+$tipo_usuario = $_SESSION['tipo_usuario'];
 include('includes/conexion.php');
 ?>
 <!DOCTYPE html>
