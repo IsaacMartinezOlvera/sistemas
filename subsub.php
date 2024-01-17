13cf0b463f455fffd00d3be028a8f61d71b599b5
EcoGarcia101 <611910454@utsalamanca.edu.mx>
2024-01-17 09:13:10 -0600

    No sé

--- subsub.php
+++ subsub.php
@@ -28,7 +28,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>DIF | Añadir Coordianción </title>
+    <title>DIF | Añadir Servicio </title>
     <link rel="stylesheet" href="assets/css/tarjeta-dist.css">
 </head>
 
