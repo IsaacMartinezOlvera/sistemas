82598dcb5d3812f92b15415c2ccd3d9fde0eee36
IsaacMartinezOlvera <612010704@ustalamancanca.edu.mx>
2024-01-22 11:04:51 -0600

    muchos cambios

--- crud.php
+++ crud.php
@@ -30,13 +30,49 @@ $tipo_usuario = $_SESSION['tipo_usuario'];
     <?php include('includes/header.php'); ?>
     <h1>RESGUARDO INTERNO</h1>
 
-    <form action="guardar.php" method="post">
+    <form action="guardar.php" method="post" enctype="multipart/form-data">
         <h2>Registro</h2>
         <div class="grupo">
-            <label for="">Área Resguardante</label>
-            <input type="text" name="" id="">
+            <label for="">Consecutivo No:</label>
+            <input type="text" name="consecutivo" id="">
 
-            <button name="submit">Registrar</button>
+            
+        </div>
+
+        <div class="grupo">
+            <label for="">Área Resguardante:</label>
+            <input type="text" name="area" id="">
+
+           
+        </div>
+
+        <div class="grupo">
+            <label for="">Selecciona una imagen:</label>
+            <input type="file" name="imagen" id="">
+        </div>
+
+        <div class="grupo">
+            <label for="">Descripción:</label>
+            <input type="text" name="descripcion" id="">
+        </div>
+
+        <div class="grupo">
+            <label for="">Características Generales:</label>
+            <input type="text" name="caracteristicas" id="">
+        </div>
+
+        <div class="grupo">
+            <label for="">Marca:</label>
+            <input type="text" name="marca" id="">
+        </div>
+
+        <div class="grupo">
+            <label for="">Modelo:</label>
+            <input type="text" name="marca" id="">
+        </div>
+
+        <div>
+        <button name="submit">Registrar</button>
         </div>
     </form>
 
