32aa3f0d551b5951d734e1156796abb804cb5aed
IsaacMartinezOlvera <612010704@ustalamancanca.edu.mx>
2024-01-22 14:11:46 -0600

    errores y mas errores

--- crud.php
+++ crud.php
@@ -30,72 +30,71 @@ $tipo_usuario = $_SESSION['tipo_usuario'];
     <?php include('includes/header.php'); ?>
     <h1>RESGUARDO INTERNO</h1>
 
-    <form action="guardar.php" method="post" enctype="multipart/form-data">
-        <h2>Registro</h2>
-        <div class="grupo">
+    <div class="contenedor">
+        <form action="guardar.php" method="post" enctype="multipart/form-data" class="crud">
+            <h2>Registro</h2>
             <label for="">Consecutivo No:</label>
             <input type="text" name="consecutivo" id="">
 
 
-        </div>
 
-        <div class="grupo">
+
+
             <label for="">Área Resguardante:</label>
             <input type="text" name="area" id="">
 
 
-        </div>
 
-        <div class="grupo">
+
+
             <label for="">Selecciona una imagen:</label>
             <input type="file" name="imagen" id="">
-        </div>
 
-        <div class="grupo">
+
+
             <label for="">Descripción:</label>
             <input type="text" name="descripcion" id="">
-        </div>
 
-        <div class="grupo">
+
+
             <label for="">Características Generales:</label>
             <input type="text" name="caracteristicas" id="">
-        </div>
 
-        <div class="grupo">
+
+
             <label for="">Marca:</label>
             <input type="text" name="marca" id="">
-        </div>
 
-        <div class="grupo">
+
+
             <label for="">Modelo:</label>
             <input type="text" name="modelo" id="">
-        </div>
 
-        <div class="grupo">
+
+
             <label for="">NO. De Serie:</label>
             <input type="text" name="serie" id="">
-        </div>
 
-        <div class="grupo">
+
+
             <label for="">Color:</label>
             <input type="text" name="color" id="">
-        </div>
 
-        <div class="grupo">
+
             <label for="">Usuario Responsable:</label>
             <input type="text" name="usuario" id="">
-        </div>
 
-        <!-- me quede aqui -->
-        <div class="grupo">
+
+            <!-- me quede aqui -->
+
             <label for="">Observaciones:</label>
-            <input type="text" name="usuario" id="">
-        </div>
+            <input type="text" name="observaciones" id="">
 
 
-        <div>
+
+
             <button name="submit">Registrar</button>
-        </div>
+    </div>
     </form>
 
 </body>
